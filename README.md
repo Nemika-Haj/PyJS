d6fb4bcfff36e721ad9ff5dc41770c990961ce29
Nemika <33802693+Nemijus@users.noreply.github.com>
2021-02-22 15:18:57 +0200

    Update README and Setup

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # **PyJS**
 Ever wanted to use JS functions in Python? Now you can! PyJS provides you with a lot of functions from JavaScript. The library is still being worked on, and any contributions are welcome.
 
+## **Installation**
+Use `python -m pip install Python.js`
+
 ## **Example**
 ```py
 from PyJS import * # you may want to do this so you don't have to add PyJS. in front of every function.
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(
-  name = 'PyJS',
+  name = 'Python.js',
   packages = ['PyJS'],
   version = '0.1',
   license='MIT',
@@ -9,14 +9,13 @@ setup(
   author_email = 'nemika@bytestobits.dev',
   url = 'https://github.com/Nemika-Haj/PyJS',
   download_url = 'https://github.com/Nemika-Haj/PyJS/archive/0.1.tar.gz',
-  keywords = ['pyjs', 'javascript', 'python', 'js'],
+  keywords = ['pyjs', 'javascript', 'python', 'js', 'py.js'],
   long_description="README.md",
   long_description_content_type="text/md",
   install_requires=[
           'forbiddenfruit',
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha'
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',
