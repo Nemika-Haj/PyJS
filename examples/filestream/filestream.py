5d221f7fe01c3e95546d458383af33d900e29f97
Nemika <33802693+Nemijus@users.noreply.github.com>
2021-02-23 13:48:42 +0200

    Updated fs

--- examples/filestream/filestream.py
+++ examples/filestream/filestream.py
@@ -1,5 +1,5 @@
 from PyJS import *
-from PyJS.modules import filestream as fs
+from PyJS.modules import fs
 
 writeStream = fs.createWriteStream("hello.txt")
 writeStream.write("Whatchu lookin' at!")
